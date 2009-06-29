2e96c055171286a7de70ca8db6e53d472d0f9b44
Brookins Consulting <info@brookinsconsulting.com>
2009-06-29 01:24:17 +0000

    Updated: Copyright information in ezinfo file.

--- ezinfo.php
+++ ezinfo.php
@@ -3,12 +3,17 @@ class bccarouselInfo
 {
     function info()
     {
+        $BCCurrentYearString = date('Y');
         return array(
-            'Name' => "<a href="http://projects.ez.no/bccarousel">BC Carousel</a>",
+            'Name' => "<a href='http://projects.ez.no/bccarousel'>BC Carousel</a>",
             'Version' => "1.0.0",
-            'Copyright' => "Copyright (C) 2008 - 2009 <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
+            'Copyright' => "Copyright (C) 2008 - $BCCurrentYearString <a href='http://brookinsconsulting.com/'>Brookins Consulting</a>",
+            'License' => "GNU General Public License v2.0 (or later)",
             'Author' => "Brookins Consulting",
-            'License' => "GNU General Public License v2.0 (or later)"
+            'Includes the following third-party software' => array( 'Name' => "<a href='http://sorgalla.com/jcarousel'>jCarousel</a> - Javascript Carousel",
+                                                                    'Version' => '0.2.3',
+                                                                    'Copyright' => 'Copyright (C) 2006 <a href="http://sorgalla.com/">Jan Sorgalla</a>',
+                                                                    'License' => ' Dual licensed under the MIT and GPL licenses' )
         );
     }
 }
