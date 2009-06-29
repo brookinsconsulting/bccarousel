664533724af78e31d84acd4b7a7f3fecbac98f30
Brookins Consulting <info@brookinsconsulting.com>
2009-06-29 01:51:35 +0000

    Updated: Commented out unnecessary menu class identifier settings.

--- settings/menu.ini.append.php
+++ settings/menu.ini.append.php
@@ -1,12 +1,12 @@
 <?php /* #?ini charset="utf-8"?
 
 [MenuContentSettings]
-# TopIdentifierList[]
-TopIdentifierList[]=carousel
-TopIdentifierList[]=carousel_list
+## TopIdentifierList[]
+# TopIdentifierList[]=carousel
+# TopIdentifierList[]=carousel_list
 
-# LeftIdentifierList[]
-LeftIdentifierList[]=carousel
-LeftIdentifierList[]=carousel_list
+## LeftIdentifierList[]
+# LeftIdentifierList[]=carousel
+# LeftIdentifierList[]=carousel_list
 
 */ ?>
